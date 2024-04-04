0dfe7e6fb42fcea3ba51b912058d2c4c0472b05f
manishkumarjha21 <166038370+manishkumarjha21@users.noreply.github.com>
2024-04-04 22:23:45 +0530

    experiments

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 const express = require("express");
 require("dotenv").config();
 const app = express();
-const cors = require('cors')
-const userRouter=require('./routes/userRouter')
+const cors = require("cors");
+const userRouter = require("./routes/userRouter");
 const cookieParser = require("cookie-parser");
-app.use(cors())
+
+const corsOptions = {
+  origin: true,
+  credentials: true,
+};
+
+app.use(cors(corsOptions)); // Enable CORS with options
+
 app.use(cookieParser());
 app.use(express.json());
-app.use("/api/v1",userRouter)
+app.use("/api/v1", userRouter);
+
 const PORT = process.env.PORT || 3000;
 require("./config/connect").connect();
+
 app.listen(PORT, () => {
-  console.log(`Server stared at port no. ${PORT}`);
+  console.log(`Server started at port no. ${PORT}`);
 });
